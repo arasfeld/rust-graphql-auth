63377ff20f25749c1bd05a331f916ea6140ec854
Adam Rasfeld <rasfelad@miamioh.edu>
2021-12-11 12:11:28 -0500

    add poem

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use poem::{
+  get, handler, listener::TcpListener, middleware::Tracing, web::Path, EndpointExt, Route, Server,
+};
+
+#[handler]
+fn hello(Path(name): Path<String>) -> String {
+    format!("Hello {}!", name)
+}
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+  let app = Route::new().at("/hello/:name", get(hello)).with(Tracing);
+  let server = Server::new(TcpListener::bind("127.0.0.1:3000"))
+    .name("rust-graphql-auth")
+    .run(app);
+  println!("🚀 Server ready at http://localhost:3000");
+  server.await
 }
