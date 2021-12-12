6e6c4f7836f28dd237167d185ff3ad913a9b6a4b
Adam Rasfeld <rasfelad@miamioh.edu>
2021-12-12 12:43:25 -0500

    add diesel & postgres

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,30 @@
 use poem::{
-  get, handler, listener::TcpListener, middleware::Tracing, web::Path, EndpointExt, Route, Server,
+    get, handler, listener::TcpListener, middleware::Tracing, web::Path, EndpointExt, Route, Server,
 };
 
 #[handler]
 fn hello(Path(name): Path<String>) -> String {
-  format!("Hello {}!", name)
+    format!("Hello {}!", name)
 }
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-  // Load environment variables from .env file
-  dotenv::dotenv().ok();
+    // Load environment variables from .env file
+    dotenv::dotenv().ok();
 
-  // Server address
-  let port = dotenv::var("PORT").unwrap();
-  let addr = format!("127.0.0.1:{}", port);
+    // Server address
+    let port = dotenv::var("PORT").unwrap();
+    let addr = format!("127.0.0.1:{}", port);
 
-  // Server
-  let app = Route::new().at("/hello/:name", get(hello)).with(Tracing);
-  let server = Server::new(TcpListener::bind(addr))
-    .name("rust-graphql-auth")
-    .run(app);
+    // Server
+    let app = Route::new().at("/hello/:name", get(hello)).with(Tracing);
+    let server = Server::new(TcpListener::bind(addr))
+        .name("rust-graphql-auth")
+        .run(app);
 
-  // Server running
-  println!("🚀 Server ready at http://localhost:{}", port);
+    // Server running
+    println!("🚀 Server ready at http://localhost:{}", port);
 
-  // Awaiting server to exit
-  server.await
+    // Awaiting server to exit
+    server.await
 }
